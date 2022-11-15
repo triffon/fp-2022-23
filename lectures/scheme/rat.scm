f8b297f8ccfd517e1e728694642eefb0b467629f
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-11-15 10:59:13 +0200

    Капсулирано рационално число

--- lectures/scheme/rat.scm
+++ lectures/scheme/rat.scm
@@ -1,25 +1,26 @@
 (load "highorder.scm")
 
 (define (make-rat n d)
-  (if (= d 0) (/ n d)
-      (let* ((g (gcd n d))
-             (n1 (quotient n g))
-             (d1 (quotient d g)))
-        (cons 'rat (if (> d1 0) (cons n1 d1) (cons (- n1) (- d1)))))))
-
+  (let ((r 
+           (if (= d 0) (/ n d)
+               (let* ((g (gcd n d))
+                      (n1 (quotient n g))
+                      (d1 (quotient d g)))
+                 (if (> d1 0) (cons n1 d1) (cons (- n1) (- d1)))))))
+    (lambda (prop)
+      (case prop
+        ('rat? #t)
+        ('get-numer (car r))
+        ('get-denom (cdr r))
+        ('print r)))))
+          
 (define (check-rat f)
   (lambda (p)
     (if (rat? p) (f p) (/ 1 0))))
 
-(define get-numer (check-rat cadr))
-(define get-denom (check-rat cddr))
-(define (rat? p) (and (pair? p)
-                      (eq? (car p) 'rat)
-                      (let ((r (cdr p)))
-                        (and (pair? r)
-                             (integer? (car r))
-                             (positive? (cdr r))
-                             (= (gcd (car r) (cdr r)) 1)))))
+(define (get-numer r) (r 'get-numer))
+(define (get-denom r) (r 'get-denom))
+(define (rat? p) (r 'rat?))
 
 (define (*rat p q)
   (make-rat (* (get-numer p) (get-numer q))
