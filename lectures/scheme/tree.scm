a9279bd82a64544b70a9458468e5aaf96450b1a6
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-11-15 11:36:23 +0200

    Реализация на двоично дърво

---
+++ lectures/scheme/tree.scm
@@ -0,0 +1,20 @@
+(define empty-tree '())
+(define empty-tree? null?)
+(define (make-tree root left right) (list root left right))
+(define root car)
+(define left cadr)
+(define right caddr)
+(define (make-leaf x) (make-tree x empty-tree empty-tree))
+
+(define t
+  (make-tree 1 (make-leaf 2)
+               (make-tree 3 (make-leaf 4)
+                            (make-leaf 5))))
+
+(define (tree? t)
+  (or (null? t)
+      (and
+       (list? t)
+       (= (length t) 3)
+       (tree? (cadr t))
+       (tree? (caddr t)))))
