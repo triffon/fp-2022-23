5cffd101d6b92a8695a4915d868848af89be4585
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-11-29 11:56:17 +0200

    Първи дефиниции на Haskell

---
+++ lectures/haskell/Basics.hs
@@ -0,0 +1,18 @@
+module Basics where
+
+-- >>> x
+-- 2
+x :: Int
+-- x = "abc"
+x = 2
+
+y :: Double
+-- >>> y
+-- 11.5
+y = fromIntegral (x^2) + 7.5
+
+z :: String
+-- >>> z
+-- "I love Haskell"
+z = "I love Haskell"
+-- z = "I love Scheme"
