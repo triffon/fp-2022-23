39c28be33798f7671693d0ba5faf02c52dbaec00
Deyan Goranov <deian.goranov@protonmail.com>
2022-12-13 00:53:31 +0200

    Add week 10

---
+++ exercises/cs2/10.haskell.lazy-lists/class-start.10.hs
@@ -0,0 +1,34 @@
+-- взима първите n елемента на списък
+take' n xs = undefined
+-- премахва първите n елемента от списък
+drop' n xs = undefined
+
+cartesian = undefined
+
+--list comprehension
+
+zip' = undefined
+zipWith' = undefined
+
+-- обещанията (stream-овете) от Scheme се изрично се оценяват лениво,
+-- в Хаскел това е по подразбиране за всички промениливи
+
+-- безкрайните списъци са просто списъци, за които може да игнорираме края им - пишем рекурсии без дъно
+--f = f
+
+from n = undefined
+
+ones = undefined
+zeroes = undefined
+repeat' = undefined
+
+nats = undefined
+fibs = undefined
+
+primes = undefined
+
+cycle' = undefined
+iterate' = undefined
+
+-- поток от Питагоровите тройки. Питагорова тройка е наредена тройка (a, b, c), за която a^2 + b^2 = c^2.
+pythagoreanTriples = undefined
---
+++ exercises/cs2/exam2-prep/README.md
@@ -0,0 +1,4 @@
+## Второто контролно през годините
+- 2021/22 https://learn.fmi.uni-sofia.bg/mod/page/view.php?id=263183
+- 2019/20 https://learn.fmi.uni-sofia.bg/mod/url/view.php?id=223861
+- Задачи от изпита 2017/18 https://docs.google.com/document/d/1U0atZZBMUMJy0nwO4yeEyY-NVmbu7037eOwmeh-95p8/edit
