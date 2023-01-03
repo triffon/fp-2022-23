b3eb20a0c210605ebe344123ccc0adaa85a01b51
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2023-01-03 12:03:57 +0200

    Примери за дефиниране на изброен тип и запис

--- lectures/haskell/Data.hs
+++ lectures/haskell/Data.hs
@@ -56,3 +56,136 @@ instance Measurable a => Measurable [a] where
 
 -- >>> :k Measurable
 -- Measurable :: * -> Constraint
+
+-- >>> :t EQ
+-- EQ :: Ordering
+
+-- >>> :k Ordering
+-- Ordering :: *
+
+type Name = String
+type Score = Int
+-- data Player = Player Name Score
+data Player = Player { name :: Name, score :: Score }
+
+-- >>> :k Name
+-- Name :: *
+
+-- >>> :k Player
+-- Player :: *
+
+-- >>> :t Player
+-- Player :: Name -> Score -> Player
+    
+emptyPlayer :: Player
+emptyPlayer = Player "" 0
+
+instance Eq Player where
+    Player n1 s1 == Player n2 s2 = n1 == n2 && s1 == s2
+
+-- >>> :t name
+-- name :: Player -> Name
+
+-- >>> :t score
+-- score :: Player -> Score
+
+katniss :: Player
+katniss = Player { name = "Katniss Everdeen", score = 45}
+
+-- >>> name katniss
+-- "Katniss Everdeen"
+
+-- data Student = Student { name :: String, year :: Int }
+
+data Shape = Circle { radius :: Double } | Rectangle { width, height :: Double }
+                deriving (Eq, Ord, Show, Read)
+
+-- >>> :k Shape
+-- Shape :: *
+
+-- >>> :t Circle
+-- Circle :: Double -> Shape
+
+-- >>> :t Rectangle
+-- Rectangle :: Double -> Double -> Shape
+
+circle :: Shape
+circle = Circle 2.3
+
+rect :: Shape
+rect = Rectangle 3.5 1.8
+
+area :: Shape -> Double
+area (Circle radius) = pi * radius^2
+area (Rectangle width height) = width * height
+
+-- >>> (area circle, area rect)
+-- (16.619025137490002,6.3)
+
+-- >>> :t radius
+-- radius :: Shape -> Double
+
+-- >>> radius rect
+-- No match in record selector radius
+
+-- TODO: inscribe, enscribe ???
+
+-- >>> circle
+-- Circle {radius = 2.3}
+
+-- >>> (read "Circle {radius = 2.3}":: Shape) == circle
+
+-- >>> :t Nothing
+-- Nothing :: Maybe a
+
+-- >>> :t Just
+-- Just :: a -> Maybe a
+
+-- >>> (read "Circle {radius = 2.3}":: Shape) == circle
+-- True
+-- >>> (read "Circle {radius = 1.2}":: Shape) < circle
+-- True
+-- >>> (read "Circle {radius = 1.2}":: Shape) > circle
+-- False
+-- >>> :k Maybe
+-- Maybe :: * -> *
+-- >>> :k Maybe Int
+-- Maybe Int :: *
+-- >>> :k Maybe Bool
+-- Maybe Bool :: *
+-- >>> :k Int -> Int
+-- Int -> Int :: *
+-- >>> :k (->)
+-- (->) :: * -> * -> *
+-- >>> :k [Int]
+-- [Int] :: *
+-- >>> :k [Bool]
+-- [Bool] :: *
+-- >>> :t []
+-- [] :: [a]
+-- >>> :k []
+-- [] :: * -> *
+-- >>> :k ([] Int)
+-- ([] Int) :: *
+-- >>> :k ([] Bool)
+-- ([] Bool) :: *
+-- >>> :k ([] ([] Int))
+-- ([] ([] Int)) :: *
+-- >>> :k (Int, Bool)
+-- (Int, Bool) :: *
+-- >>> :k (,) Int Bool
+-- (,) Int Bool :: *
+-- >>> :k (,)
+-- (,) :: * -> * -> *
+-- >>> :t (,)
+-- (,) :: a -> b -> (a, b)
+-- >>> :k (,) [Int] (Bool -> Bool)
+-- (,) [Int] (Bool -> Bool) :: *
+-- >>> :k (,) Int
+-- (,) Int :: * -> *
+-- >>> :k (->) Int
+-- (->) Int :: * -> *
+-- >>> :k (->) Int Bool
+-- (->) Int Bool :: *
+-- >>> :k (->) Int String
+-- (->) Int String :: *
